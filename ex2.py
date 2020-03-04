676af28b15f59bb853048662cc49ac4f4a327e6c
Slurmster <smcowell@gmail.com>
2020-03-03 19:05:07 -0700

    This is the second exercise in LPTHW

---
+++ ex2.py
@@ -0,0 +1,11 @@
+# A comment, this is so you can read your program later
+#Anything after this # is ignored by python.
+
+print ("I could have code like this.") # and the comment is ignored
+
+# You can also use a comment to "Disable" or comment out code:
+# print ("This won't run.")
+
+print ("This will run")
+
+
