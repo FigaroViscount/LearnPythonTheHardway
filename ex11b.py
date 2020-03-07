ac61aaaefc48a585d25cd6041646734af6f9c1b0
Slurmster <smcowell@gmail.com>
2020-03-07 14:23:07 -0700

    This is another exercise for this chapter.

---
+++ ex11b.py
@@ -0,0 +1,9 @@
+print("Is long is your penis?", end= '')
+penis = input()
+print("Where do you live?", end= '')
+address = input()
+print("What type of car do you have?",  end= '')
+car = input()
+
+print(f"So your penis is {penis} you live at {address} and you have a {car}.")
+
