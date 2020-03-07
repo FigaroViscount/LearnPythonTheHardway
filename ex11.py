220f94a4956674f4c612bdd6c93bb4ff38509f04
Slurmster <smcowell@gmail.com>
2020-03-07 14:10:54 -0700

    This is the 11th exercise

---
+++ ex11.py
@@ -0,0 +1,10 @@
+print("How old are you?", end= ' ' )
+age = input()
+print("How tall are you?" , end=' ' )
+height = input()
+print("How much to you weight?" , end=' ')
+weight = input()
+
+print(f"So, your {age} old, {height} tall and {weight} heavy.")
+
+
