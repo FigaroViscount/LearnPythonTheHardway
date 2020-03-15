b69fa1e103f601703402ad5a07405b82672c692d
Slurmster <smcowell@gmail.com>
2020-03-15 09:56:59 -0700

    Look I wrote a program all by myself! sc23.py

---
+++ sc23.py
@@ -0,0 +1,13 @@
+rad = input("Please enter the radius: ")
+
+Area = float(rad) * float(rad) * (3.1416)
+
+print("Three area of the circle is", Area)
+
+volume = ((float(rad) * float(rad) * float(rad) * 3.1416) * 4) /3 
+
+print("The volume of a globe is", volume)
+
+
+
+
