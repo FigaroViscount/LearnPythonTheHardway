4b357a6f1901694c5fe833f4befefadc129b71bc
Slurmster <smcowell@gmail.com>
2020-03-15 10:24:41 -0700

    More programs

---
+++ sc20.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("Percent chance JOsh will run this program: ", first)
+print("Percent chance Josh will give a lame excuse for not running this: ", second
+)
+print("Percent chance Josh would give me absolute shit if I did not do this homework ", third)
+
+
---
+++ sc21.py
@@ -0,0 +1,6 @@
+penis = input("How many penises do you have? ")
+nipples = input("How many nipples do you have? ")
+vaginas = input("How many vaginas do you have? ")
+
+print(f"So you have {penis} penises, {nipples} nipples and {vaginas} vaginas!")
+
