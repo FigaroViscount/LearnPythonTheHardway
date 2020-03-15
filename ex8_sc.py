a51abf02fae1a270515e26f089c410f2771a887f
Slurmster <smcowell@gmail.com>
2020-03-14 19:54:13 -0700

    Making my own programs

---
+++ ex8_sc.py
@@ -0,0 +1,12 @@
+formatter = " {} {} {} {} {} "
+
+print(formatter.format(1, 2, 3, 4, 5))
+print(formatter.format("one", "two", "three", "four", "Five"))
+print(formatter.format("goat", "big", "Pig", "Cow", "Wow"))
+print(formatter.format(
+	"There once was an instructor name Josh,",
+	"\vWho thought that his penis was posh,", 
+	"\vHe Jumped in a crowd,",
+	"\vAnd Screamed right out loud,",
+	"\v...Actually he didn't, because he is not reading his student's programs."
+))
