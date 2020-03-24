4a511d2ec1d30c3cf5c7a4171f72978f7f966721
Slurmster <smcowell@gmail.com>
2020-03-23 21:20:05 -0700

    Kowabunga!

--- ex35.py
+++ ex35.py
@@ -3,7 +3,7 @@ from sys import exit
 def gold_room():
 	print("This room is full of gold.  How much do you take?")
 
-	choice = raw_input("> ")
+	choice = input("> ")
 
 	if "0" in choice or "1" in choice:
 		how_much = int(choice)
@@ -13,6 +13,8 @@ def gold_room():
 	if how_much < 50:
 		print("Nice, you're not greedy, you win!")
 		exit(0)
+	else: 
+		dead("You greedy Bastard!")
 
 def bear_room():
 	print("There is a bear here.")
